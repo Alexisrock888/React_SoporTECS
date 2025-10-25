92f9279c248ccb0a1cc5556ca843aacc2a747711
Alexisrock888 <alexisrock888@gmail.com>
2025-10-25 14:57:14 -0500

    Actualizacion de comentarios

--- src/IndexApp.jsx
+++ src/IndexApp.jsx
@@ -1,6 +1,6 @@
-import HeaderApp from "./Header/HeaderApp"; //npm start en terminal para ejecutar el proyecto
-import FooterApp from "./Footer/FooterApp"; //npm run deploy para desplegar el proyecto
-import "./Styles/styles.css";
+import HeaderApp from "./Header/HeaderApp"; //npm start en terminal para ejecutar el proyecto.
+import FooterApp from "./Footer/FooterApp"; //npm run deploy para desplegar el proyecto.
+import "./Styles/styles.css";               //luego para guardar cambios en la rama  damos en el mas para subir los cambios agregamos comentari y damos un ctyl enter, carga y confirmar.
 import { Outlet } from "react-router-dom";
 
 const IndexApp = () => {
